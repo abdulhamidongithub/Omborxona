238bd4efc79c306b6c32344568c0f837de7bf69e
Abdulkhamid Egamberdiev <87631879+abdulhamidongithub@users.noreply.github.com>
2023-02-25 08:15:41 +0300

    Update urls.py
    Added the url for all clients

--- asosiyapp/urls.py
+++ asosiyapp/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', BolimlarView.as_view(), name='bolimlar'),
     path('mahsulotlar/', MahsulotlarView.as_view(), name='mahsulotlar'),
+    path('clientlar/', ClientView.as_view(), name='clientlar'),
     path('pr_delete/<int:pk>/', ProductDeleteView.as_view(), name='m-ochirish'),
     path('pr_update/<int:pk>/', MahsulotUpdateView.as_view(), name='m-update'),
 ]
